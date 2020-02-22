2be0b8ce3412e67e8a5df6b33ab0fd375aa9737b
paris <suppersoft@gmail.com>
2020-02-22 23:24:13 +0800

    登录、注册

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,42 @@
-
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="email=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="screen-orientation" content="portrait">
+    <meta name="full-screen" content="yes">
+    <meta name="browsermode" content="application">
+    <meta name="x5-orientation" content="portrait">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="x5-page-mode" content="app">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1">
+    <meta name="renderer" content="webkit">
+    <title>登录页——中国加油，武汉加油</title>
+    <link rel="stylesheet" href="/css/basic.css">
+    <link rel="stylesheet" href="/css/login.css">
+    <script>
+        function resize() {
+            if (document.documentElement.clientWidth > 750) {
+                // document.documentElement.removeAttribute("style");
+                document.documentElement.style.fontSize = "50px";
+                return;
+            }
+            document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + "px";
+        }
+        resize();
+        window.onresize = resize;
+    </script>
+</head>
+<body>
+<nav class="nav">
+    中国加油，武汉加油
+</nav>
 @if ($errors->any())
-    <div class="alert alert-danger">
+    <div class="alert alert-danger" style="font-size: 16px;text-align: center;color: red;">
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -8,12 +44,21 @@
         </ul>
     </div>
 @endif
-
-<form method="post" action="">
-    @csrf
-    手机：<input name="phone"/><br/>
-    密码：<input type="password" name="password"/><br/>
-    <input type="submit" value="提交"/>
-</form>
-
-<a href="/register">注册</a>
+<div class="wrap">
+    <h3 class="formTitle">中国加油，武汉加油</h3>
+    <form id="loginForm" class="loginForm" method="post" action="">
+        <div class="formGroup">
+            <input class="formInput" type="text" placeholder="手机号" name="phone">
+            <p class="formTip">请输入手机号</p>
+        </div>
+        <div class="formGroup">
+            <input class="formInput" type="password" placeholder="密码" name="password">
+            <p class="formTip">请输入密码</p>
+            @csrf
+        </div>
+        <button class="loginBtn" type="submit">登录</button>
+    </form>
+    <button class="loginBtn" type="submit">注册</button>
+</div>
+</body>
+</html>
