1f8ff026b755b0c7848d020983148f56a9fe0662
paris <suppersoft@gmail.com>
2020-02-22 20:59:22 +0800

    路由规划与controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pariswang
+ * Date: 2020/2/22
+ * Time: 8:35 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index(Request $request)
+    {
+        echo 'index';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 首页
+Route::get('/', 'IndexController@index');
+
+// 查找房源（酒店列表）
+Route::get('hotel_list', 'HotelController@list');
+
+// 查看申请（没有指定酒店的申请单列表）
+Route::get('apply_list', 'ApplyController@list');
+
+// 用户注册（医护、志愿者、后勤保障人员）
+Route::get('register', 'UserController@register')->name('user_register');
+
+// 有对应酒店的申请
+Route::get('apply_hotel', 'ApplyController@apply_hotel');
+
+// 无对应酒店的申请
+Route::get('apply', 'ApplyController@apply');
