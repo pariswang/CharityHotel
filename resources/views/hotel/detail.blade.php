e5a52bca52d007eabe2cf6c31146b4f7592deeeb
paris <suppersoft@gmail.com>
2020-02-23 21:51:27 +0800

    酒店详情

--- resources/views/hotel/detail.blade.php
+++ resources/views/hotel/detail.blade.php
@@ -8,15 +8,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>酒店名称</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">酒店名称</div>
-            </div>
-        </div>
-    </van-cell-group>
-    <van-cell-group>
-        <div class="van-cell van-field">
-            <div class="van-cell__title van-field__label"><span>酒店简介</span></div>
-            <div class="van-cell__value">
-                <div class="van-field__body --text-left">酒店简介</div>
+                <div class="van-field__body --text-left">{{$hotel->hotel_name}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -24,7 +16,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>酒店类型</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">酒店类型</div>
+                <div class="van-field__body --text-left">{{$hotel->classify}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -32,7 +24,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>酒店三餐</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">酒店三餐</div>
+                <div class="van-field__body --text-left">{{$hotel->meal}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -40,7 +32,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>酒店地址</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">酒店地址</div>
+                <div class="van-field__body --text-left">{{$hotel->address}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -48,7 +40,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>联系人</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">联系人</div>
+                <div class="van-field__body --text-left">{{$hotel->uname}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -56,7 +48,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>联系电话</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">联系电话</div>
+                <div class="van-field__body --text-left">{{$hotel->phone}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -64,7 +56,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>可安排房间数</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">可安排房间数</div>
+                <div class="van-field__body --text-left">{{$hotel->room_count}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -72,7 +64,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>医护人员免费</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">免费/不免费</div>
+                <div class="van-field__body --text-left">{{$hotel->medical_staff_free ? '免费' : '不免费'}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -80,7 +72,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>愿意被征用</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">愿意/不愿意</div>
+                <div class="van-field__body --text-left">{{$hotel->expropriation ? '愿意' : '不愿意'}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -88,7 +80,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>前台接待</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">有/没有</div>
+                <div class="van-field__body --text-left">{{$hotel->reception ? '有' : '没有'}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -96,7 +88,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>客房清洁服务</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">提供/不提供</div>
+                <div class="van-field__body --text-left">{{$hotel->cleaning ? '有' : '没有'}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -104,7 +96,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>房间配置说明</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">房间配置说明</div>
+                <div class="van-field__body --text-left">{{$hotel->collocation_description}}</div>
             </div>
         </div>
     </van-cell-group>
@@ -112,7 +104,7 @@
         <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>酒店说明</span></div>
             <div class="van-cell__value">
-                <div class="van-field__body --text-left">酒店说明</div>
+                <div class="van-field__body --text-left">{{$hotel->description}}</div>
             </div>
         </div>
     </van-cell-group>
