d13548f5f0e91fb596a7f25cd80e14342adbc91f
paris <suppersoft@gmail.com>
2020-02-23 18:52:10 +0800

    添加医护人员入口

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,9 @@
 <div class="page" id="index">
     <van-button type="primary" round block url="/apply_list">查看申请</van-button>
     <van-button type="primary" round block url="/hotel_list">查找房源</van-button>
-    <van-button type="primary" round block url="/admin/auth/login">酒店入口</van-button>
-    <van-button type="primary" round block url="/admin/auth/login">管理入口</van-button>
+    <van-button type="primary" round block url="/login">医护人员入口</van-button>
+    <van-button type="primary" round block url="/admin/auth/login">酒店人员入口</van-button>
+    <van-button type="primary" round block url="/admin/auth/login">系统管理入口</van-button>
 </div>
 @endsection
 @section('js')
