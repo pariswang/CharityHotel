fb077d28de647f94f6b775aaac75cb64cb1b0845
paris <suppersoft@gmail.com>
2020-02-23 23:09:41 +0800

    隐藏系统管理入口

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@ html,body,#app{ height:100%;}
     <van-button type="primary" round block url="/hotel_list">查找房源</van-button>
     <van-button type="primary" round block url="/login">医护人员入口</van-button>
     <van-button type="primary" round block url="/admin/auth/login">酒店人员入口</van-button>
-    <van-button type="primary" round block url="/admin/auth/login">系统管理入口</van-button>
+    {{--<van-button type="primary" round block url="/admin/auth/login">系统管理入口</van-button>--}}
 </div>
 @endsection
 @section('js')
