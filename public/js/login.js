e50317cbe3e2a4e17a9ee884561768ae42d9480a
kermit <455196886@qq.com>
2020-02-23 18:49:33 +0800

    处理异步错误

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@
  * @Author: kermit.yu 
  * @Date: 2020-02-22 22:13:41 
  * @Last Modified by: kermit.yu
- * @Last Modified time: 2020-02-23 11:05:01
+ * @Last Modified time: 2020-02-23 18:46:31
  */
 
 new Vue({
@@ -44,7 +44,18 @@ new Vue({
                     }else{
                         window.location.href = '/hotel_list';
                     }
-
+                },
+                error: function (res) {
+                    var errors = res.responseJSON.errors;
+                    console.log('errors', errors);
+                    var errors_text = '';
+                    for(var i in  errors) {
+                        var item = errors[i];
+                        item.forEach(function (_item) { 
+                            errors_text += _item;
+                        });
+                    }
+                    vant.Notify({ type: 'danger', message: errors_text !== '' ? errors_text : '登录错误，请重试'});
                 },
                 complete: function () {
                     _this.submitLoading = false;
--- public/js/register.js
+++ public/js/register.js
@@ -2,7 +2,7 @@
  * @Author: kermit.yu 
  * @Date: 2020-02-22 22:13:41 
  * @Last Modified by: kermit.yu
- * @Last Modified time: 2020-02-23 15:23:08
+ * @Last Modified time: 2020-02-23 18:44:35
  */
 
 new Vue({
@@ -71,6 +71,18 @@ new Vue({
                     }else{
                         window.location.href = '/hotel_list';
                     }
+                },
+                error: function (res) {
+                    var errors = res.responseJSON.errors;
+                    console.log('errors', errors);
+                    var errors_text = '';
+                    for(var i in  errors) {
+                        var item = errors[i];
+                        item.forEach(function (_item) { 
+                            errors_text += _item;
+                        });
+                    }
+                    vant.Notify({ type: 'danger', message: errors_text !== '' ? errors_text : '注册错误，请重试'});
                 },
                 complete: function () {
                     _this.submitLoading = false;
