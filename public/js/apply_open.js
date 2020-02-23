205a1bfa1759f8f898e5601d4fc1cfceab15f01e
kermit <455196886@qq.com>
2020-02-23 15:00:24 +0800

    修改成功跳转

--- public/js/apply_open.js
+++ public/js/apply_open.js
@@ -2,7 +2,7 @@
  * @Author: kermit.yu 
  * @Date: 2020-02-22 22:13:41 
  * @Last Modified by: kermit.yu
- * @Last Modified time: 2020-02-23 14:21:11
+ * @Last Modified time: 2020-02-23 14:59:42
  */
 
 new Vue({
@@ -92,7 +92,7 @@ new Vue({
                     console.log('res', res);
                     vant.Notify({ type: 'success', message: '申请成功' });
                     setTimeout( function() {
-                        window.location.href = '/hotel_list';
+                        window.location.href = '/apply_list';
                     }, 1000);
                 },
                 complete: function () {
