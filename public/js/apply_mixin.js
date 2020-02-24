de8dd66eb33a10d4f21f982248f7514f07828203
kermit <455196886@qq.com>
2020-02-24 17:12:07 +0800

    申请单增加我的医院

---
+++ public/js/apply_mixin.js
@@ -0,0 +1,162 @@
+/*
+ * @Author: kermit.yu 
+ * @Date: 2020-02-22 22:13:41 
+ * @Last Modified by: kermit.yu
+ * @Last Modified time: 2020-02-24 17:11:16
+ */
+
+var HOSPITALS_COLUMNS = {};
+_.each(REGIONS, function (item, index) {
+    // console.log(item);
+    if (item.hospitals.length > 0) {
+        HOSPITALS_COLUMNS[item.region_name] = _.pluck(item.hospitals, 'hospital_name');
+    }
+});
+console.log('HOSPITALS_COLUMNS', HOSPITALS_COLUMNS);
+
+var apply = {
+    data: {
+        conn_person: CONN_PERSON,
+        conn_phone: CONN_PHONE,
+        conn_position: CONN_POSITION,
+        conn_company: CONN_COMPANY,
+        checkin_num: 1,
+        room_count: 1,
+        date_begin: moment().format('YYYY/MM/DD'),
+        date_end_min: new Date(moment().add(1, 'days').format('YYYY/MM/DD')),
+        date_end_default: new Date(moment().add(1, 'days').format('YYYY/MM/DD')),
+        date_end: '',
+        can_pay: true,
+        has_letter: true,
+        hotel_id: ppo.getUrlParam('id'),
+        hospitals: [],
+        hospitals_columns : [
+            { values: _.keys(HOSPITALS_COLUMNS) },
+            { values: HOSPITALS_COLUMNS[_.keys(HOSPITALS_COLUMNS)[0]] }
+        ],
+        hospitalPicker: false,
+        showDateBeginPicker: false,
+        showDateEndPicker: false,
+        submitLoading: false
+    },
+    methods: {
+        formatDate: function(date) {
+            return `${date.getMonth() + 1}/${date.getDate()}`;
+        },
+        dateBeginOnConfirm: function (date) {
+            console.log('dateBegin', date);
+            this.date_begin = moment(date).format('YYYY/MM/DD');
+            this.date_end_min = new Date(moment(date).add(1, 'days').format('YYYY/MM/DD')),
+            this.date_end_default = new Date(moment(date).add(1, 'days').format('YYYY/MM/DD')),
+            this.showDateBeginPicker = false;
+        },
+        dateEndOnConfirm: function (date) {
+            console.log('dateEnd', date);
+            this.date_end = moment(date).format('YYYY/MM/DD');
+            this.showDateEndPicker = false;
+        },
+        hospitalonChange: function(picker, values) {
+            picker.setColumnValues(1, HOSPITALS_COLUMNS[values[0]]);
+        },
+        hospitalOnConfirm: function(value, index) {
+            // console.log('value', value, index);
+            var hospital = REGIONS[index[0]].hospitals[index[1]];
+            // console.log('hospital', hospital);
+            this.hospitals.push({
+                id: hospital.id,
+                hospital_name: hospital.hospital_name,
+                region_id: hospital.region_id
+            });
+            this.hospitalPicker = false;
+        },
+        // 删除医院
+        deleteHospital: function (id) {
+            this.hospitals = _.filter(this.hospitals, function (item) {return item.id !== id });
+        },
+        // 提交申请
+        submitApply: function (data) {
+            var _this = this;
+            if(this.conn_person == ''){
+                vant.Toast('联系人不能为空');
+                return false;
+            }
+            if(this.conn_phone == ''){
+                vant.Toast('联系人电话不能为空');
+                return false;
+            } else if (!/^1\d{10}$/.test(this.conn_phone)){
+                vant.Toast('请填写正确的联系人电话');
+                return false;
+            }
+            if(this.conn_position == ''){
+                vant.Toast('职位不能为空');
+                return false;
+            }
+            if(this.conn_company == ''){
+                vant.Toast('工作单位不能为空');
+                return false;
+            }
+            if(this.hospitals.length<1){
+                vant.Toast('我的医院不能为空');
+                return false;
+            }
+            // 过滤掉医院名
+            var _hospitals = _.map(this.hospitals, function(item){
+                var _item = {
+                    id: item.id,
+                    region_id: item.region_id
+                }
+                return _item;
+            });
+            var defaultData = {
+                _token: $('input[name="_token"]').val(),
+                conn_person: this.conn_person,
+                conn_phone: this.conn_phone,
+                conn_position: this.conn_position,
+                conn_company: this.conn_company,
+                checkin_num: this.checkin_num,
+                room_count: this.room_count,
+                date_begin: this.date_begin,
+                date_end: this.date_end,
+                can_pay: this.can_pay,
+                has_letter: this.has_letter,
+                hospitals: _hospitals,
+            }
+            if (data) {
+                for(var i in data){
+                    defaultData[i] = data[i];
+                }
+            }
+            console.log('defaultData', defaultData);
+            $.ajax({
+                type: 'POST',
+                url: '/apply_hotel',
+                data: defaultData,
+                beforeSend: function () {
+                    _this.submitLoading = true;
+                },
+                success: function (res) {
+                    console.log('success', res);
+                    vant.Notify({ type: 'success', message: '申请成功' });
+                    setTimeout( function() {
+                        window.location.href = '/hotel_list';
+                    }, 1000);
+                },
+                error: function (res) {
+                    var errors = res.responseJSON.errors;
+                    console.log('errors', errors);
+                    var errors_text = '';
+                    for(var i in  errors) {
+                        var item = errors[i];
+                        item.forEach(function (_item) { 
+                            errors_text += _item;
+                        });
+                    }
+                    vant.Notify({ type: 'danger', message: errors_text !== '' ? errors_text : '发布错误，请重试'});
+                },
+                complete: function () {
+                    _this.submitLoading = false;
+                }
+            });
+        }
+    }
+};
--- resources/views/apply/hotel.blade.php
+++ resources/views/apply/hotel.blade.php
@@ -62,7 +62,7 @@
             <div class="van-cell__value">
                 <div class="van-field__body --space-between">
                     <span v-text="date_begin"></span>
-                    <van-icon name="arrow-down"/>
+                    <van-icon name="arrow-down" size="18"/>
                 </div>
             </div>
         </div>
@@ -74,7 +74,7 @@
             <div class="van-cell__value">
                 <div class="van-field__body --space-between">
                     <span v-text="date_end !== '' ? date_end : '请选择离店时间'"></span>
-                    <van-icon name="arrow-down"/>
+                    <van-icon name="arrow-down" size="18"/>
                 </div>
             </div>
         </div>
@@ -100,25 +100,41 @@
             </div>
         </div>
     </van-cell-group>
+    <van-cell-group>
+        <div class="van-cell van-cell--required van-field">
+            <div class="van-cell__title van-field__label"><span>我的医院</span></div>
+            <div class="van-cell__value">
+                <div class="van-field__body --column">
+                    <div class="value___item" v-for="hospital in hospitals" key="hospital.id" >
+                        <span v-text="hospital.hospital_name"></span>
+                        <van-icon name="delete" color="#ee0a24" size="18" @click="deleteHospital(hospital.id)"/>
+                        <!-- <van-button icon="delete" plain type="danger" size="mini"/> -->
+                    </div>
+                    <div class="value___item" @click="hospitalPicker = true">
+                        <span>请增加医院</span>
+                        <van-icon name="plus" size="18"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <van-popup v-model="hospitalPicker" position="bottom">
+            <van-picker
+                show-toolbar
+                title="增加医院"
+                :columns="hospitals_columns"
+                @change="hospitalonChange"
+                @cancel="hospitalPicker = false"
+                @confirm="hospitalOnConfirm"
+            />
+        </van-popup>
+    </van-cell-group>
     <van-button class="submit-btn" type="primary" round block :loading="submitLoading" loading-text="申请中..." @click="onSubmit">申请</van-button>
-<!-- <form method="post" action="">
-    <input name="conn_person" value="{{$user->uname}}"/><br/>
-    <input name="conn_phone" value="{{$user->phone}}"/><br/>
-    <input name="conn_position" value="{{$user->position}}"/><br/>
-    <input name="conn_company" value="{{$user->company}}"/><br/>
-    <input name="checkin_num" /><br/>
-    <input name="room_count"/><br/>
-    <input name="date_begin"/><br/>
-    <input name="date_end"/><br/>
-    <input name="can_pay"/><br/>
-    <input name="has_letter"/><br/>
-    <input type="submit" value="提交"/>
-    <input name="hotel_id" type="hidden" value="{{$hotel->id}}"/>
-</form> -->
 </div>
 @endsection
 @section('js')
 <script>
+    var REGIONS = {!! $regions->toJson() !!};
+    console.log('REGIONS', REGIONS);
     var CONN_PERSON = '{!! $user->uname !!}',
         CONN_PHONE = '{!! $user->phone !!}',
         CONN_POSITION = '{!! $user->position !!}',
@@ -128,5 +144,6 @@
     console.log('CONN_POSITION', CONN_POSITION);
     console.log('CONN_COMPANY', CONN_COMPANY);
 </script>
+<script src="{{asset('/js/apply_mixin.js')}}"></script>
 <script src="{{asset('/js/apply_hotel.js')}}"></script>
 @endsection
