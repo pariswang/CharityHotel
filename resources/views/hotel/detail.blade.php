3e47865698e65cdcf29152217f1de4f525077dbe
paris <suppersoft@gmail.com>
2020-02-26 01:39:07 +0800

    酒店遗失了2个字段

--- resources/views/hotel/detail.blade.php
+++ resources/views/hotel/detail.blade.php
@@ -30,6 +30,14 @@
     </van-cell-group>
     <van-cell-group>
         <div class="van-cell van-field">
+            <div class="van-cell__title van-field__label"><span>所在地区</span></div>
+            <div class="van-cell__value">
+                <div class="van-field__body --text-left">{{$hotel->region->region_name}}</div>
+            </div>
+        </div>
+    </van-cell-group>
+    <van-cell-group>
+        <div class="van-cell van-field">
             <div class="van-cell__title van-field__label"><span>酒店地址</span></div>
             <div class="van-cell__value">
                 <div class="van-field__body --text-left">{{$hotel->address}}</div>
@@ -65,6 +73,14 @@
             <div class="van-cell__title van-field__label"><span>医护人员免费</span></div>
             <div class="van-cell__value">
                 <div class="van-field__body --text-left">{{$hotel->medical_staff_free ? '免费' : '不免费'}}</div>
+            </div>
+        </div>
+    </van-cell-group>
+    <van-cell-group>
+        <div class="van-cell van-field">
+            <div class="van-cell__title van-field__label"><span>折扣价格</span></div>
+            <div class="van-cell__value">
+                <div class="van-field__body --text-left">{{$hotel->discount_price}}</div>
             </div>
         </div>
     </van-cell-group>
