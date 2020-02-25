66e4a195e59ce54a86037e77dec371dfaf5d7fbb
Simon Zhao <simonzhaox@gmail.com>
2020-02-25 17:11:15 +0800

    修改手机版个人显示

--- resources/views/vendor/admin/partials/header.blade.php
+++ resources/views/vendor/admin/partials/header.blade.php
@@ -34,7 +34,8 @@
                         <!-- The user image in the navbar-->
                         <!-- <img src="{{ Admin::user()->avatar }}" class="user-image" alt="User Image"> -->
                         <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                        <span class="hidden-xs">{{ Admin::user()->name }}</span>
+                        {{ Admin::user()->name }}
+                        <span class="hidden-xs"></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
