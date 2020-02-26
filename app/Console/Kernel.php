b6730e1238ef9027673b42efb958b6490ab974ae
paris <suppersoft@gmail.com>
2020-02-26 15:28:39 +0800

    定时运行

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('apply:index')->everyFiveMinutes();
+        $schedule->command('hotel:index')->everyFiveMinutes();
     }
 
     /**
