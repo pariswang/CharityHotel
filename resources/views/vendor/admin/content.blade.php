d9b0a889fbe64bb278af841240d74b74dc09f407
Simon Zhao <simonzhaox@gmail.com>
2020-03-03 18:06:57 +0800

    增加后台首页面包屑

--- resources/views/vendor/admin/content.blade.php
+++ resources/views/vendor/admin/content.blade.php
@@ -40,7 +40,8 @@
         </ol>
         @elseif(config('admin.enable_default_breadcrumb'))
         <ol class="breadcrumb" style="margin-right: 30px;">
-            <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i> {{__('Home')}}</a></li>
+            <li><a href="/"><i class="fa fa-newspaper-o"></i> 首页</a></li>
+            <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i> 后台</a></li>
             @for($i = 2; $i <= count(Request::segments()); $i++)
                 <li>
                 {{ucfirst(Request::segment($i))}}
