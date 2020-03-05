344a3360b55a9f904ee691b907e46d3678497ce1
huabin.hong <121826725@qq.com>
2020-03-05 20:46:49 +0800

    医护端发布的订单列表，每条记录加入取消订单的操作

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,6 +6,7 @@
         <van-cell title="{{$user->uname}}" label="{{$user->phone}}" />
         {{--<van-icon name="edit" size="20"/>--}}
     </van-cell-group>
+    @csrf
     <van-panel title="我的申请单" style="margin-top:1em;">
         <van-tabs @click="changeTab" color="#1e63cb">
             <van-tab title="已发布">
@@ -27,10 +28,12 @@
                                 {{$apply->conn_company}} {{$apply->checkin_num}}名 {{$apply->conn_position}} 急需酒店。
                                 联系人：{{$apply->conn_person}}
                             </div>
-                            <div class="item-ft">
-                                <van-button color="#1d63cb" size="small" round plain>查看详情</van-button>
-                            </div>
                         </a>
+                            <div class="item-ft">
+                                <van-button color="#1d63cb" style="margin-right: 10px;" size="small" round @click="cancelApply({{$apply->id}})">取消申请</van-button>  
+                        <a href="/apply_detail?id={{$apply->id}}">
+                                <van-button color="#1d63cb" size="small" round plain>查看详情</van-button></a>
+                            </div>
                     </div>
                 @empty
                     <div class="item-empty">
@@ -90,6 +93,46 @@ new Vue({
     methods: {
         changeTab: function (name, title) {
             console.log('title', title);
+        }
+        ,cancelApply: function (applyid) {
+            var _this = this;
+            function beforeClose(action, done) {
+                if (action === 'confirm') {
+                    $.ajax({
+                        type: 'POST',
+                        url: '/apply/cancel',
+                        data: {
+                            _token: $('input[name="_token"]').val(),
+                            id: applyid,
+                        },
+                        success: function (res) {
+                            done();
+                            window.location.href = '/profile';
+                        },
+                        error: function (res) {
+                            var errors = res.responseJSON.errors;
+                            var errors_text = '';
+                            for(var i in  errors) {
+                                var item = errors[i];
+                                item.forEach(function (_item) { 
+                                    errors_text += _item;
+                                });
+                            }
+                            vant.Notify({ type: 'danger', message: errors_text !== '' ? errors_text : '登录错误，请重试'});
+                        },
+                        complete: function () {
+                            done();
+                        }
+                    });
+                } else {
+                    done();
+                }
+            }
+            vant.Dialog.confirm({
+                title: '提示',
+                message: '你确定要取消这个申请单？',
+                beforeClose
+            });
         }
     }
 });
