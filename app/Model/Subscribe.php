aa2f67280dfc93f588082363dbf3a2231a741185
paris <suppersoft@gmail.com>
2020-03-09 18:12:25 +0800

    bugfix：spare_phone 被guard了

--- app/Model/Subscribe.php
+++ app/Model/Subscribe.php
@@ -18,7 +18,7 @@ class Subscribe extends Model
     protected $table = 'wh_subscribe';
 
     protected $fillable = [
-        'user_id', 'conn_person', 'conn_phone', 'checkin_num', 'date_begin', 'date_end', 'createdate', 'hotel_id', 'conn_position', 'conn_company', 'room_count', 'can_pay', 'has_letter', 'status', 'admin_id', 'region_id','hoteltaking_date','hide_status'
+        'user_id', 'conn_person', 'conn_phone', 'checkin_num', 'date_begin', 'date_end', 'createdate', 'hotel_id', 'conn_position', 'conn_company', 'room_count', 'can_pay', 'has_letter', 'status', 'admin_id', 'region_id','hoteltaking_date','hide_status','spare_phone'
     ];
 
     public $timestamps = false;
